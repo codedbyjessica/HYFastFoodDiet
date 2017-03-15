c2209f5f1221a16611a9eaf574b74987e4d4993d
Jessica Zhang <yaojiazhang@gmail.com>
2017-03-15 10:22:47 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("its workingggggggggggg");
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+	render() {
+		return(
+			<div>
+			
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
